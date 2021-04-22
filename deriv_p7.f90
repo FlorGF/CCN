3cc5e752ffb9002e6a5fe3a23b5698c5e34671ca
GuidoCerioni <60760576+GuidoCerioni@users.noreply.github.com>
2021-04-22 16:22:20 -0300

    22/4

--- deriv_p7.f90
+++ deriv_p7.f90
@@ -9,10 +9,12 @@ real(8), dimension(2) :: deriv
 !problema 2
 !deriv(1)=y(2)
 !deriv(2)=0.15*y(1)
-end function
 !problema 7 
 deriv(1)=y(2)
-deriv(2)=x+3+2*x*y(1)-x**2*y(2)
+deriv(2)=x+3.0+2.0*x*y(2)-y(1)*x**2
+end function
+!problema 7 
+
 
 
 end module 
--- p7e7.f90
+++ p7e7.f90
@@ -6,6 +6,20 @@ real(8), dimension(2) :: x, y, yp
 integer(8) :: N , i
 real(8), allocatable, dimension(:,:):: res 
 
+open(unit=10, file="datosp7e7.dat",status="old")
+read(10,*)x
+read(10,*)y
+read(10,*)yp 
+read(10,*)N 
+close(10)
+allocate(res(n+1,3))
+call disparo(x, y, yp, N, res)
+
+do i=1,N+1 
+   if(abs(res(i,1)-0.5)<0.001) write(*,*)"El valor en 0.5 es", res(i,2)
+   if(abs(res(i,1)-1.5)<0.001) write(*,*)"El valor en 1.5 es", res(i,2)
+enddo 
+
 
 
 
